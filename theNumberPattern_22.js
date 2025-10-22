3cf05c1d5db74ac5523581d7424556eaa452742a
Arijitkhan99git <arijitkhan52@gmail.com>
2025-10-22 15:55:01 +0530

    Completed number 22 problem

--- theNumberPattern_22.js
+++ theNumberPattern_22.js
@@ -1,29 +1,22 @@
-let n = 4
+let n = 5
 let max= n*2-1
-let temp1=0, temp2=0;
-let temp3=max-1, temp4=max-1;
-let count=n;
+let top, bottom,right,left;
+
 
 for(i=0; i<max; i ++){
     let row = ""
     for(j=0; j<max; j++){
-        if(temp1==i || temp2==j || temp3==i || temp4==j)
-        {
-           
-            count=n;
-           
-        }else{
-           count=count-1
-        }
-          
-         row+=`${count}`
-       
+      top=i;
+      bottom=(2*n-2)-i;
+      right= (2*n-2)-j;
+      left=j;
+    
+      let value = n-Math.min(top, bottom, right, left)
+      row+= `${value}`
+
     }
+
     console.log(row);
     
-        temp1=i+1
-        temp2=j+1
-        // temp3=temp3-1
-        // temp4=temp4-1
         
 }
